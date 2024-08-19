fe4afb58656bbf91563c92333673f6e457891b2d
FarukKeskinsoy <133097328+codeaddev@users.noreply.github.com>
2024-08-19 16:46:27 +0300

    ........

--- app/components/CollapsibleCard.jsx
+++ app/components/CollapsibleCard.jsx
@@ -19,11 +19,11 @@ export default function CollapsibleCard({ item,imgSrc }) {
         <Link
             href={`urun-gruplari/kategori?id=${item?.id}`}
             ref={cardRef}
-            className={`flex relative rounded min-h-[325px] flex-1 hover:flex-[2] transition-all duration-300 ease-in-out cursor-pointer w-full bos`}
+            className={`border flex relative rounded min-h-[325px] flex-1 hover:flex-[2] transition-all duration-300 ease-in-out cursor-pointer w-full bos`}
             //onMouseEnter={handleMouseEnter}  // Fetch data on hover
             //onMouseLeave={handleMouseLeave}  // Hide results on mouse leave
         >
-            <img src={imgSrc} className={`w-full h-full !object-cover absolute rounded ${relatedGroup.length>0&&hovered?"opacity-20":"opacity-50"} -z-10`}/>
+            <img src={imgSrc} className={`w-full h-full !object-cover absolute rounded ${relatedGroup.length>0&&hovered?"opacity-30":"opacity-60"} -z-10`}/>
             <h2 className="text-xl font-semibold py-4">{item?.title}</h2>
             {/* {loading && <div className="absolute inset-0 bg-blue-100 opacity-50"></div>} */}
             {/* {error && <p>Error: {error.message}</p>} */}
