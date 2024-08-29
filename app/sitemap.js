8bc2de943a41a4f087a05ada395333cb199c7073
FarukKeskinsoy <133097328+codeaddev@users.noreply.github.com>
2024-08-29 13:37:10 +0300

    sitemap3

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,14 +1,49 @@
-import { MetadataRoute } from 'next'
+import { collection, getDocs } from 'firebase/firestore';
+import { MetadataRoute } from 'next';
 
-export default function sitemap(){
-  // Add logic to dynamically generate URLs (e.g., from a CMS or API)
-  return [
-    {
-      url: 'https://parsanalitik.com/blog',
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.9,
-    },
-    // Include additional dynamic URLs
-  ];
+export default async function sitemap() {
+  const collections = ['Blogs', 'Products','Sectors','Brands','ProductGroups']; // Array of collection names
+  const allPaths = [];
+
+  for (const collectionName of collections) {
+    const col = collection(db, collectionName);
+    const snapshot = await getDocs(col);
+
+    const paths = snapshot.docs.map(doc => {
+      const data = doc.data();
+      let basePath;
+
+      // Define the base path based on collection name
+      if (collectionName === 'Blogs') {
+        basePath = '/blog';
+      } else if (collectionName === 'Products') {
+        basePath = '/urunler';
+      } else if (collectionName === 'Sectors') {
+        basePath = '/sektorler';
+      }else if (collectionName === 'Brands') {
+        basePath = '/temsilcilikler';
+      }else if (collectionName === 'ProductGroups') {
+        basePath = '/urun-gruplari';
+      }
+
+      return {
+        url: `${basePath}/${data?.url}-${data?.id}`, // Construct the URL
+        lastModified: new Date(), // Modify based on your data
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      };
+    });
+
+    allPaths.push(...paths);
+  }
+
+  // Include the homepage
+  allPaths.push({
+    url: 'https://parsanalitik.com',
+    lastModified: new Date(),
+    changeFrequency: 'daily',
+    priority: 1.0,
+  });
+
+  return allPaths;
 }
