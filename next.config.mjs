a033bd42b53d8ba5527167ab135e7a25b23da0a7
FarukKeskinsoy <133097328+codeaddev@users.noreply.github.com>
2024-08-29 11:37:39 +0300

    29agu2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  
+  async redirects() {
+    return [
+      {
+        source: '/blog/:id-',
+        destination: '/blog/:id-:id',
+        permanent: true, // Indicates a 301 permanent redirect
+      },
+    ];
+  },
     experimental: {
         missingSuspenseWithCSRBailout: false,
       },
